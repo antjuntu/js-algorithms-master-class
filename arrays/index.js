80f9a974179300cb13240aeee32ee0c0b3011b13
antjuntu <antjuntu@gmail.com>
2018-11-15 16:52:30 +0200

    arrays

---
+++ arrays/index.js
@@ -0,0 +1,28 @@
+let names = ['Michael', 'Melissa', 'Andrea']
+console.log(names)
+
+// push pop
+
+// shift unshift
+
+// concat
+
+// slice
+
+// splice
+
+// sort
+
+// forEach map filter reduce etc.
+
+let first = names.shift()
+console.log('first', first)
+console.log(names)
+
+let length = names.unshift('Paavo')
+console.log('length', length)
+console.log(names)
+
+console.log('pop:')
+const popped = names.pop()
+console.log(popped, names)
